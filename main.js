929a808b1d7157b0a0d1a0aed00d4e1a312aafd8
sophie-apples <sophieannemurray@gmail.com>
2020-06-05 12:14:26 +0000

    temporary fix for null value in database

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function getData (url, cb) {
+function getData(url, cb) {
     var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
@@ -23,12 +23,12 @@ function getTableHeaders(obj) {
 
 function generatePaginationbuttons(next, prev) {
     if (next && prev) {
-        return `<button onclick="writeToDocument('${prev}')">Previous</button></button>
-                <button onclick="writeToDocument('${next}')">Next</button></button>`;
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
     } else if (next && !prev) {
-        return `<button onclick="writeToDocument('${next}')">Next</button></button>`;
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
     } else if (!next && prev) {
-        return `<button onclick="writeToDocument('${prev}')">Previous</button></button>`;
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
     }
 }
 
@@ -38,6 +38,7 @@ function writeToDocument(url) {
 
     getData(url, function(data) {
         var pagination = "";
+        console.log(data)
 
         if (data.next || data.previous) {
             pagination = generatePaginationbuttons(data.next, data.previous);
@@ -46,16 +47,25 @@ function writeToDocument(url) {
         var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item) {
+            console.log(item)
             var dataRow = [];
 
             Object.keys(item).forEach(function(key) {
-                var rowData = item[key].toString();
+                // if/else Statement added for when a field has a null value.
+                if (item[key] == null){
+                    var rowData = "unknown";
+                }else{
+                    var rowData = item[key].toString();
+                }
+                
+                // The line below will work once database is fixed
+                // var rowData = item[key].toString();
+                console.log(rowData);
                 var truncatedData = rowData.substring(0, 15);
                 dataRow.push(`<td>${truncatedData}</td>`)
             });
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
-
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g,"");
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
     });
 }
